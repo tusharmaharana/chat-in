dc4a540be16a633aec57a8ee25a3b1f6f140557f
Tushar <tusharmaharana6@gmail.com>
2021-11-30 19:04:26 +0530

    feat: socket.io setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,68 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import keys from "./config/keys";
 
-const app: Application = express();
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
+const users: any = {};
+const socketToRoom: any = {};
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  })
+);
+app.enable("trust proxy");
+
+io.on("connection", socket => {
+  socket.on("join room", roomID => {
+    if (users[roomID]) {
+      // const length = users[roomID].length;
+      // if (length === 4) {
+      //   socket.emit("room full");
+      //   return;
+      // }
+      users[roomID].push(socket.id);
+    } else {
+      users[roomID] = [socket.id];
+    }
+    socketToRoom[socket.id] = roomID;
+    const usersInThisRoom = users[roomID].filter((id: any) => id !== socket.id);
+
+    socket.emit("all users", usersInThisRoom);
+  });
+
+  socket.on("sending signal", payload => {
+    io.to(payload.userToSignal).emit("user joined", { signal: payload.signal, callerID: payload.callerID });
+  });
+
+  socket.on("returning signal", payload => {
+    io.to(payload.callerID).emit("receiving returned signal", { signal: payload.signal, id: socket.id });
+  });
+
+  socket.on("disconnect", () => {
+    const roomID = socketToRoom[socket.id];
+    let room = users[roomID];
+    if (room) {
+      room = room.filter((id: any) => id !== socket.id);
+      users[roomID] = room;
+    }
+    socket.broadcast.emit("user left", socket.id);
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Yo Man, you're there keep it up and always remember NOTHING LASTS FOREVER");
 });
 
-app.listen(5000, () => console.log("server running"));
+httpServer.listen(keys.port, () => console.log(`server is running on ${keys.port}`));
