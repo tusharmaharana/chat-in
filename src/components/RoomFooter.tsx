6c5b117a294fdbddaf21bb0b7a185d7a16fb7576
Tushar <tusharmaharana6@gmail.com>
2021-12-04 19:10:27 +0530

    feat(roomfooter.tsx): added end call functionality

--- src/components/RoomFooter.tsx
+++ src/components/RoomFooter.tsx
@@ -1,23 +1,34 @@
 import React, { ReactElement, useState } from "react";
+import { useNavigate } from "react-router";
+import { Socket } from "socket.io-client";
 
 interface Props {
   userVideo: React.MutableRefObject<HTMLVideoElement>;
+  socketRef: React.MutableRefObject<Socket>;
 }
 
-const RoomFooter: React.FC<Props> = ({ userVideo }: Props): ReactElement => {
+const RoomFooter: React.FC<Props> = ({ userVideo, socketRef }: Props): ReactElement => {
   const [audioMute, setAudioMute] = useState<boolean>(false);
   const [videoHide, setVideoHide] = useState<boolean>(false);
   const stream = userVideo?.current?.srcObject as MediaStream;
+  const navigate = useNavigate();
 
-  // console.log(stream.getVideoTracks()[0]);
+  // console.log(stream.getTracks());
 
   const handleAudioClick = (): void => {
     stream.getAudioTracks()[0].enabled = audioMute;
     setAudioMute((prev: boolean) => !prev);
   };
+
   const handleVideoClick = (): void => {
     stream.getVideoTracks()[0].enabled = videoHide;
     setVideoHide((prev: boolean) => !prev);
+  };
+
+  const handleEndClick = (): void => {
+    stream.getTracks().forEach(track => track.stop());
+    socketRef.current.disconnect();
+    navigate("/");
   };
 
   return (
@@ -27,6 +38,9 @@ const RoomFooter: React.FC<Props> = ({ userVideo }: Props): ReactElement => {
       </button>
       <button type="button" onClick={handleVideoClick}>
         {videoHide ? "show video" : "hide video"}
+      </button>
+      <button type="button" onClick={handleEndClick}>
+        end call
       </button>
     </div>
   );
