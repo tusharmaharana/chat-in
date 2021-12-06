097e9ed3947a5661b5bc499174f20527b22c685b
Tushar <tusharmaharana6@gmail.com>
2021-12-06 16:41:37 +0530

    feat: added join with code functionality

--- src/components/CreateRoom.tsx
+++ src/components/CreateRoom.tsx
@@ -1,19 +1,38 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import { useNavigate } from "react-router";
 import { v4 as uuidV4 } from "uuid";
 
 const CreateRoom = (): ReactElement => {
+  const [value, setValue] = useState<string>(undefined);
+  // setMounted(true);
   const navigate = useNavigate();
 
-  const handleClick = (): void => {
+  const handleCreateClick = (): void => {
     const id = uuidV4();
     navigate(`/${id}`);
   };
 
+  const handleJoinClick = (event: React.MouseEvent): void => {
+    event.preventDefault();
+    navigate(`/${value}`);
+  };
+
   return (
-    <button onClick={handleClick} type="button">
-      Create room
-    </button>
+    <div>
+      <button type="button" onClick={handleCreateClick}>
+        Create room
+      </button>
+      <form>
+        <input
+          placeholder="Enter a code or link"
+          value={value}
+          onChange={(event): void => setValue(event.target.value)}
+        />
+        <button type="submit" onClick={(event): void => handleJoinClick(event)}>
+          Join
+        </button>
+      </form>
+    </div>
   );
 };
 
