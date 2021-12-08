83f5fcfaddc35d989e5a3652b8f8a1e16d8ab2ed
Tushar <tusharmaharana6@gmail.com>
2021-12-08 19:39:56 +0530

    feat: added styles to VideoPlayer

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import React, { ReactElement, useEffect, useRef } from "react";
 import { Instance } from "simple-peer";
 import { IPeers } from "../../types";
@@ -9,6 +10,7 @@ interface Props {
 
 interface VideoProps {
   peer: Instance;
+  size: string;
 }
 
 const Video = (props: VideoProps): ReactElement => {
@@ -21,22 +23,57 @@ const Video = (props: VideoProps): ReactElement => {
   }, []);
 
   return (
-    <video playsInline autoPlay ref={ref} height="300" width="300">
-      <track kind="captions" />
-    </video>
+    <StyledDiv videoWidth={props.size}>
+      <StyledVideo playsInline autoPlay ref={ref}>
+        <track kind="captions" />
+      </StyledVideo>
+    </StyledDiv>
   );
+};
+
+const renderVideoSize = (count: number): string => {
+  const rem: number = 90 / count;
+  if (rem === 90) return "90%";
+  if (rem >= 22 && rem < 90) return "44%";
+  return "32%";
 };
 
 const VideoPlayer: React.FC<Props> = (props: Props) => {
   const { userVideo, totalPeers } = props;
+  const count: number = (totalPeers?.length ?? 0) + 1;
+  const size: string = renderVideoSize(count);
   return (
-    <div>
-      <video muted ref={userVideo} autoPlay playsInline height="300" width="300" />
+    <Container>
+      <StyledDiv videoWidth={size}>
+        <StyledVideo muted ref={userVideo} autoPlay playsInline />
+      </StyledDiv>
       {totalPeers.map((peer: IPeers) => {
-        return <Video key={peer.peerID} peer={peer.peer} />;
+        return <Video key={peer.peerID} peer={peer.peer} size={size} />;
       })}
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 0.7rem;
+  overflow-x: hidden;
+  overflow-y: scroll;
+`;
+
+const StyledVideo = styled.video`
+  width: 100%;
+`;
+
+const StyledDiv = styled.div`
+  width: ${(props: { videoWidth: string }): string => props.videoWidth};
+  margin: 0.4rem 0.5rem 0.1rem 0.5rem;
+  overflow: hidden;
+`;
 
 export default VideoPlayer;
